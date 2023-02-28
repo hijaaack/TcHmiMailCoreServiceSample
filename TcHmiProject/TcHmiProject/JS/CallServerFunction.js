8f3c1671edc12d715ea8fc06cbd28e72f3077413
hijaack <jack@borelius.se>
2023-02-28 11:59:44 +0100

    init

---
+++ TcHmiProject/TcHmiProject/JS/CallServerFunction.js
@@ -0,0 +1,61 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.758.8/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var TcHmiProject;
+        (function (TcHmiProject) {
+            function CallServerFunction(FunctionName, Parameter) {
+
+                let request = {
+                    'requestType': 'ReadWrite',
+                    'commands': [{
+                        'symbol': FunctionName,
+                        'commandOptions': ['SendErrorMessage']
+                    }]
+                };
+
+                //Set request write object
+                request.commands[0].writeValue = Parameter;
+
+                //Check if the websocket to the server is ready
+                if (TcHmi.Server.isWebsocketReady()) {
+                    //Send request to the TwinCAT HMI Server
+                    TcHmi.Server.request(request, function (data) {
+
+                        //Check callback data for errors
+                        if (data.error !== TcHmi.Errors.NONE) {
+                            //Handle Server error here
+                            TcHmi.Log.error(TcHmi.Log.buildMessage(data.error), false);
+                            return;
+                        }
+
+                        //Check response for errors
+                        if (data.response.error !== undefined) {
+                            //Handle HMI server repsonse error here
+                            TcHmi.Log.warn(TcHmi.log.buildMessage(data.response.error), false);
+                            return;
+                        }
+
+                        //If no commands are defined in the response return
+                        if (!data.response.commands || !Array.isArray(data.response.commands)) return;
+
+                        //Check all commands in the response for errors
+                        for (var i = 0; i < data.response.commands.length; i++) {
+                            //console.log(data);
+                            let errorObject = data.response.commands[i].error;
+                            if (errorObject) {
+                                //Handle HMI Server command level error here                             
+                            }
+                        }
+
+                    });
+                }
+
+            }
+            TcHmiProject.CallServerFunction = CallServerFunction;
+        })(TcHmiProject = Functions.TcHmiProject || (Functions.TcHmiProject = {}));
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
+TcHmi.Functions.registerFunctionEx('CallServerFunction', 'TcHmi.Functions.TcHmiProject', TcHmi.Functions.TcHmiProject.CallServerFunction);
---
+++ TcHmiProject/TcHmiProject/JS/mail/SendMail.js
@@ -0,0 +1,33 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017 and higher.
+/// <reference path="./../../../Packages/Beckhoff.TwinCAT.HMI.Framework.12.758.8/runtimes/native1.12-tchmi/TcHmi.d.ts" />
+
+(function (/** @type {globalThis.TcHmi} */ TcHmi) {
+    var Functions;
+    (function (/** @type {globalThis.TcHmi.Functions} */ Functions) {
+        var TcHmiProject;
+        (function (TcHmiProject) {
+            function SendMail(To, Message) {
+
+                var jsonObj = {
+                    "to": To,
+                    "from": "john@doe.com",
+                    "htmlBody": true,
+                    "body": Message,
+                    "subject": "TwinCAT HMI"
+                };
+
+                var sendFunction = TcHmi.Functions.getFunction('CallServerFunction');
+
+                if (sendFunction) {
+                    //call the server request function
+                    sendFunction('MailCoreService.SendMail', jsonObj);
+                }
+
+
+
+            }
+            TcHmiProject.SendMail = SendMail;
+        })(TcHmiProject = Functions.TcHmiProject || (Functions.TcHmiProject = {}));
+    })(Functions = TcHmi.Functions || (TcHmi.Functions = {}));
+})(TcHmi);
+TcHmi.Functions.registerFunctionEx('SendMail', 'TcHmi.Functions.TcHmiProject', TcHmi.Functions.TcHmiProject.SendMail);
